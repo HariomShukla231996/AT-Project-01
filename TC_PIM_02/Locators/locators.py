164c139e24d47ef6288abd44a30dd38d89843fe1
HariomShukla231996 <152177600+HariomShukla231996@users.noreply.github.com>
2024-01-25 21:28:54 -0800

    Add files via upload

---
+++ TC_PIM_02/Locators/locators.py
@@ -0,0 +1,10 @@
+class Web_Locators:
+
+
+    username_locator = "username"
+    password_locator = "password"
+    submit_button_locator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+    pim_locator = "//span[normalize-space()='PIM']"
+    edit_button_locator = "//div[@role='rowgroup']//div[2]//div[1]//div[9]//div[1]//button[2]//i[1]"
+    employee_middle_locator = "//input[@placeholder='Middle Name']"
+    save_button_locator = "//div[@class='orangehrm-horizontal-padding orangehrm-vertical-padding']//button[@type='submit'][normalize-space()='Save']"
